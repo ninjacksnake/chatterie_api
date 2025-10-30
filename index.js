42886c1240c7a0b0486ad4b4652d5ef3db8cec5d
Michael Fermin <ninjacksnake@gmail.com>
2025-10-29 20:15:05 -0400

    Improved backend logic

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const socket = require("socket.io");
-const http = require("http");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoutes");
 const messagesRoutes = require ('./routes/messagesRoutes');
